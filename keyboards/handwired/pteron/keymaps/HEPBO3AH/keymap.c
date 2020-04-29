8a2a824b846a58eb4b37d8aa4017c1641a8f493d
dusan.vasiljevic <dusan.vasiljevic@gmx.com>
2020-04-30 09:32:39 +1200

    Move Function keys

--- keyboards/handwired/pteron/keymaps/HEPBO3AH/keymap.c
+++ keyboards/handwired/pteron/keymaps/HEPBO3AH/keymap.c
@@ -27,18 +27,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_QWERTY] = LAYOUT( \
-  KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-                     KC_LGUI, KC_SPC,  LOWER,   KC_MUTE,         KC_MPLY, LOWER,   RAISE,   KC_LALT  \
+  GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
+  KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+                      KC_LGUI, KC_SPC,  LOWER,   KC_MUTE,         KC_MPLY, LOWER,   RAISE,   KC_LALT  \
 ),
 
 /* Lower
  * +-----------------------------------------+      +-----------------------------------------+
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |      |      |      |      |      |      |      |      |      |      |  F11 |  F12 | Del  |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -48,18 +48,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,           KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,  \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-                    _______, _______, _______, KC__VOLDOWN,     KC__VOLUP, _______, _______, _______  \
+  GRAVE_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,   \
+  KC_GRAVE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, KC_F11,  KC_F12,  KC_DEL,  \
+  _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+                      _______, _______, _______, KC__VOLDOWN,     KC__VOLUP, _______, _______, _______  \
 ),
 
 /* Raise
  * +-----------------------------------------+      +-----------------------------------------+
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * |  ESC |      |      |   [  |   ]  |  +   |      |      |      |  UP  |      |      | Del  |
+ * |  `   |      |      |   [  |   ]  |  +   |      |      |      |  UP  |  F11 |  F12 | Del  |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |      |      |      |   (  |   )  |  =   |      |      | LEFT | DOWN | RIGHT|      |      |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -69,18 +69,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_RAISE] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  \
-  KC_ESC,  XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_PPLS,     XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, KC_DEL,  \
-  _______, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_PEQL,     XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PMNS,     KC_UNDS, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, _______, \
-                    _______, KC_LALT, _______, KC_BRID,     KC_BRIU, _______, _______, _______  \
+  GRAVE_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_BSPC,   \
+  KC_GRAVE,  XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_PPLS,     XXXXXXX, XXXXXXX, KC_UP,   KC_F11,   KC_F12,  KC_DEL,  \
+  _______,   XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_PEQL,     XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, \
+  _______,   XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PMNS,     KC_UNDS, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, _______, \
+                      _______, KC_LALT, _______, KC_BRID,     KC_BRIU, _______, _______, _______  \
 ),
 
 /* Adjust (Lower + Raise)
  * +-----------------------------------------+      +-----------------------------------------+
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |      |      |      |      |      |  Del |
+ * |  `   | Reset| Debug|      |      |      |      |      |      |      |  F11 |  F12 | Del  |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -90,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_ADJUST] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  _______, RESET,   DEBUG,   _______, _______, _______,         _______, _______, _______, _______, _______, KC_DEL,  \
-  _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, \
-                    _______, _______, _______, _______,         _______, _______, _______, _______  \
+  GRAVE_ESC, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,   \
+  KC_GRAVE,  RESET,   DEBUG,   _______, _______, _______,        _______, _______, _______, KC_F11,  KC_F12,  KC_DEL,  \
+  _______,   _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, \
+  _______,   _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, \
+                      _______, _______, _______, _______,        _______, _______, _______, _______  \
 )
 
 };
